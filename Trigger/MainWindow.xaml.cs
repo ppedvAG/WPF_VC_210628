d0946047247c974aa9c0a23e6c514b04367d3bea
KlaasHagge <klaash@ppedv.de>
2021-06-30 15:39:10 +0200

    UserControls

--- Trigger/MainWindow.xaml.cs
+++ Trigger/MainWindow.xaml.cs
@@ -21,23 +21,30 @@ namespace Trigger
     /// </summary>
     public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        //Property, auf die der DataTrigger reagiert
+        private bool boolVal;
+        public bool BoolVal
+        {
+            get { return boolVal; }
+            //Setter mit Event-Wurf
+            set { boolVal = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(BoolVal))); }
+        }
+
+
         public MainWindow()
         {
             InitializeComponent();
 
+            this.BoolVal = true;
+
+            //Setzen des DataContext
             this.DataContext = this;
         }
 
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        private bool boolval;
-        public bool BoolVal
-        {
-            get { return boolval; }
-            set { boolval = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(BoolVal))); }
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
+        //EventHandler zum Ändern der Property
+        private void Btn_Ändern_Click(object sender, RoutedEventArgs e)
         {
             BoolVal = !BoolVal;
         }
